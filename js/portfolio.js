929a1ebb78ed1b41ad6e6ee4899015ac52cb46e9
Eritas <katya.butenko.job@gmail.com>
2016-01-12 12:08:42 +0200

    Jquery changed to local. Script files moved to js folder.

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,4 +1,28 @@
 /**
  * Created by Katya on 23.12.2015.
  */
-console.info('portfolio.js works');
+var contentDivs = $(".content");
+
+var allButtons = $(".btn");
+
+function hidePreviousContent(showDiv, activeButton) {
+    contentDivs.hide();
+    showDiv.show();
+    allButtons.removeClass("active");
+    activeButton.addClass("active");
+};
+
+var contactBtn = $("#contacts-btn");
+contactBtn.click(function () {
+    console.debug("contact btn clicked");
+    var contactsDiv = $("#contacts");
+    hidePreviousContent(contactsDiv, contactBtn);
+});
+
+var homeBtn = $("#home-btn");
+homeBtn.click(function () {
+    console.debug("home btn clicked");
+    var homeDiv = $("#welcome");
+    hidePreviousContent(homeDiv, homeBtn);
+});
+
